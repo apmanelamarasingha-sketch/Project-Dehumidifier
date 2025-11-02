766ba1dfffda58df29c17c095607b105ad2a469e
M K V T Amarasinghe <a.p.manelamarasingha@gmail.com>
2025-11-02 15:47:04 +0530

    Test humidity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <Arduino.h>
+#include "DHTesp.h"
 
-const int fan_1 = 4;
+const int DHT_PIN = 15;  // Change this to match your wiring
+const int FAN = 4;
+bool reucing;
+DHTesp dht;
 
 void setup() {
-  pinMode(fan_1, OUTPUT);
-  digitalWrite(fan_1, HIGH);
+  Serial.begin(115200);
+  delay(1000);
+  
+  // Initialize DHT sensor
+  dht.setup(DHT_PIN, DHTesp::DHT11);
+
+  pinMode(FAN, OUTPUT);
+  
+  Serial.println("DHT11 Humidity Sensor Test");
+  Serial.println("-------------------------");
 }
 
 void loop() {
-  delay(5000);
-  digitalWrite(fan_1, LOW);
-  delay(5000);
-  digitalWrite(fan_1, HIGH);
+  // Wait a few seconds between measurements
+  delay(dht.getMinimumSamplingPeriod());
+
+  // Reading temperature and humidity
+  float humidity = dht.getHumidity();
+  
+  if (isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Relative Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  }
+  
+  delay(2000);  // Wait 2 seconds before next reading
 }
